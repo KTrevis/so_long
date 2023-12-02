eb034edae0cf216d30ed25befebf1b7d1f4cee68
ketrevis <ketrevis@42.student.fr>
2023-12-02 13:56:39 +0100

    map file extension check

--- Makefile
+++ Makefile
@@ -5,12 +5,15 @@ SRCS = $(addprefix srcs/,$(C_FILES))
 
 CFLAGS = -Wall -Wextra -Werror
 
+LIBS = ft_printf/libftprintf.a libft/libft.a
+DEPS = $(addprefix libs/,$(LIBS))
+
 all: $(NAME)
 
 $(NAME):
 	make -C libs/mlx
 	make -C libs/ft_printf
-	cc $(CFLAGS) $(SRCS) -I includes/ -L libs/mlx -l mlx -I mlx -lXext -lX11 -lm -lz -o $(NAME) libs/ft_printf/libftprintf.a
+	cc $(CFLAGS) $(SRCS) -I includes/ -L libs/mlx -l mlx -I mlx -lXext -lX11 -lm -lz -o $(NAME) $(DEPS)
 
 fclean:
 	rm -f $(NAME)
--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,15 @@
 /*   By: ketrevis <ketrevis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 12:33:26 by ketrevis          #+#    #+#             */
-/*   Updated: 2023/12/02 13:21:38 by ketrevis         ###   ########.fr       */
+/*   Updated: 2023/12/02 13:56:26 by ketrevis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
+#include "libft.h"
 #include "so_long.h"
 
-void	init_game(t_game *game)
+static void	init_game(t_game *game)
 {
 	game->mlx = mlx_init();
 	game->window = mlx_new_window(game->mlx, 1920 / 2, 1080 / 2, "");
@@ -20,22 +22,25 @@ void	init_game(t_game *game)
 	mlx_loop(game->mlx);
 }
 
-int	args_invalid(int ac, char **av)
+static int	map_extension_invalid(char *str)
 {
-	char	*str;
 	int	i;
 
+	i = ft_strlen(str) - 1;
+	while (i >= 0 && str[i] != '.')
+		i--;
+	return (ft_strncmp(".ber", str + i, 4));
+}
+
+static int	args_invalid(int ac, char **av)
+{
 	if (ac < 2)
 		return (ft_printf("No map path given as argument."));
 	if (ac > 2)
 		return (ft_printf("Too many arguments."));
+	if (map_extension_invalid(av[1]))
+		return (ft_printf("Map extension invalid"));
 	return (0);
-	str = av[1];
-	i = 0;
-	while (str[i])
-	{
-		i++;
-	}
 }
 
 int	main(int ac, char **av)
